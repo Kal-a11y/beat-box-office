dbaa50afa2da6978d3302f0a98c8b042dcaab850
Kaila <kailaamia@gmail.com>
2023-10-30 18:57:52 -0500

    Added function to get trending movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,27 @@
 let movieBaseUrl = 'https://api.simkl.com/movies';
 let movieKeyParam = '?client_id=' + movieApiKey;
 
-
+//Gets trending movies for this week
+function getTrendingMovies(){
+    let trendingPath = '/trending/'
+    let trendingParams = '&interval=week&extended=title,overview'
+    let amountOfMovies = 10;
+    fetch(movieBaseUrl + trendingPath + movieKeyParam + trendingParams)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        let movieList = []
+        for (let i = 0; i < amountOfMovies; i++){
+            let  movie = {
+                title : data[i].title,
+                overview : data[i].overview,
+                posterUrl : `https://wsrv.nl/?url=https://simkl.in/posters/${data[i].poster}_w.webp`,
+            }
+            movieList.push(movie)
+        } 
+    })
+}
 
 
 
