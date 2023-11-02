45d4e1451916b37974156d4302d980b3c0c78ef0
Kaila <kailaamia@gmail.com>
2023-11-02 00:40:46 -0500

    Added functions to store and show favorite media

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@ let movieKeyParam = '?client_id=' + movieApiKey;
 let amountOfMovies = 5;
 
 init(); 
+$('.not-favorite-btn').on('click',favoriteMedia)
 
 //Initializes when page is opened.
 function init(){
     //put function to call games here
     getTrendingMovies();
+    showFavoriteData();
 }
 
 //Gets trending movies for this week
@@ -50,8 +52,49 @@ function showMovieData(movieList){
     }
 }
 
+function showFavoriteData(){
+    let media = JSON.parse(localStorage.getItem('Favorites'))
+    let favoriteContainer = $('#favorite-cards')
+    favoriteContainer.empty()
+    if (media !== null){
+        mediaList = media;
+        for (let i = 0; i < mediaList.length; i++) {
+            //Create card with info
+            let newCard = $(`<div class="group relative"><div  class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">  <img src="${media[i].poster}"    alt="Collection of four insulated travel bottles on wooden shelf."    class="h-full w-full object-cover object-center"></div><h3 class="mt-6 text-sm text-gray-500">  <a href="#">    <span class="absolute inset-0"></span>${media[i].title}</a> </h3><p class="text-base font-semibold text-gray-900">${media[i].overview}</p><button class="favorite-btn">❤️ Favorite</button></div>`)
+            
+            //Add card to favorite containter
+            favoriteContainer.append(newCard)
+        }
 
+    }
 
+        
+}
+function favoriteMedia(){
+    //Get media card
+    media = $(this).parent();
+    //Save data from this card
+    let item = {
+        poster :media.children().eq(0).children().attr('src'),
+        title : media.children().eq(1).text(),
+        overview : media.children().eq(2).text()
+    }
+
+    $(this).removeClass('not-favorite-btn');
+    $(this).addClass('favorite-btn');
+    $(this).text('❤️ Favorite')
+    
+    if (mediaList !== null){
+        for (let i = 0; i < mediaList.length; i++) {
+            if(mediaList[i].title === item.title){
+                return
+            } 
+        }
+    }
+    mediaList.push(item)
+    localStorage.setItem('Favorites',JSON.stringify(mediaList))
+    showFavoriteData();
+}
 
 //Uncompleted code to be used later
 // function getMoviesByGenre(genre){
