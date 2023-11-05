8274afab6294e923cd1dee7ecb0b5e4f66179a7d
rtocastro <ricktorres@live.com>
2023-11-05 13:57:21 -0800

    added functionality to modal in js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById("modal1")
+var buttons = document.getElementById("modalbutton")
+var buttons2 = document.getElementById("modalbutton1")
+
+buttons.addEventListener("click", function(){
+modal.setAttribute("class", "hidden")
+})
+
+buttons2.addEventListener("click", function(){
+    modal.setAttribute("class", "hidden")
+    })
