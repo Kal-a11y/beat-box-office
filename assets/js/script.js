68f9cddafaacd76f2f2ba2a2eb70898cf19f93bf
rtocastro <ricktorres@live.com>
2023-11-05 20:06:58 -0800

    added comments for buttons on script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var modal = document.getElementById("modal1")
 var buttons = document.getElementById("modalbutton")
 var buttons2 = document.getElementById("modalbutton1")
 
+//this is for what to do for the button functions so that when a button is clicked the modal goes away
 buttons.addEventListener("click", function(){
 modal.setAttribute("class", "hidden")
 })
