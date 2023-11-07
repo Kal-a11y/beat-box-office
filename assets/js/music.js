690e5061818e907a6b4b7e0468c3149313528ac5
Kaila <kailaamia@gmail.com>
2023-11-07 01:19:35 -0600

    Fix subheading bug2

--- assets/js/music.js
+++ assets/js/music.js
@@ -178,7 +178,7 @@ function createMusicCard(item){
                 <div  class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
                     <img src="${item.posterUrl}" alt="Collection of four insulated travel bottles on wooden shelf." class="h-full w-full object-cover object-center">
                 </div>
-                <h3 class="mt-6 text-sm text-gray-500">Artist: ${item.artist}</h3>
+                <h3 class="mt-6 text-sm text-gray-500">Artist: ${item.artist }</h3>
                 <p class="text-base font-semibold text-gray-900">${item.title}</p>
 
             </div>
